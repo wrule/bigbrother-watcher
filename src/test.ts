4752c637976128a7af60f79d829ea0abe857581c
jimao <jimao@jimaodeMacBook-Pro.local>
2021-07-20 12:49:19 +0800

    v0.1.0

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import BigBrotherWatcher from './index';
 
+// 需要准备好token
 const token = fs.readFileSync('./keys/token.key', 'utf8');
 
 const watcher = new BigBrotherWatcher(token);
